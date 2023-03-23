cc5c095bdbf0981255b4086474449a71de734616
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2023-03-23 16:51:36 +0500

    Add files via upload

---
+++ Assign#07/app.js
@@ -0,0 +1,85 @@
+//*********************CHAPTER # 26-30 ************************/
+
+//Task 1
+
+// var user = prompt("Enter Your Positive Integar");
+// var num = Number(user);
+// var rndOf = Math.round(user);
+// var floor = Math.floor(user);
+// var ceil = Math.ceil(user);
+
+
+//  document.write("Number: "+ num + "<br>" + "Round off value: "+ rndOf + 
+//  "<br>" + "Floor Value: " + floor + "<br>" + "Ceil Value: " + ceil )
+
+
+//Task 2
+
+// var user = prompt("Enter Your Positive Integar");
+// var num = Number(user);
+// var rndOf = Math.round(user);
+// var floor = Math.floor(user);
+// var ceil = Math.ceil(user);
+
+
+//  document.write("Number: "+ num + "<br>" + "Round off value: "+ rndOf + 
+//  "<br>" + "Floor Value: " + floor + "<br>" + "Ceil Value: " + ceil )
+
+//  Math.abs()
+
+// Task 3
+// var user = prompt("Enter a Absoute value");
+// var name = Math.abs(user);
+// console.log(name)
+
+
+//Task 4
+// var value = "321456";
+// var name = Math.random(value);
+// var imprNum = (name * 6) + 1;
+// var num = Math.floor(imprNum)
+
+// document.write("Random Dice Value is: " + num)
+
+
+
+//Task 5
+// var name = Math.random();
+// var num = (name * 2) +1 ;
+// var impNum = Math.floor(num);
+// if(impNum === 1){
+//     document.write(impNum + "<br>");
+//     document.write("Random Coin Value: " , "Heads")
+// }
+// else if(impNum === 2){
+//     document.write(impNum + "<br>");
+//     document.write("Random Coin Value: " , "Tails")
+   
+// }
+
+// document.write(impNum);
+
+
+// Task 6
+// var value = Math.random();
+// var num = (value * 100) +1;
+// var valueNum = Math.floor(num);
+// document.write("Random Number Between 1 to 100: "+valueNum)
+
+// //Task 7
+// var Age = prompt("Enter your Weight in kilograms");
+// // var num = parseFloat(Age) + 8;
+// console.log(num)
+
+//Task 8
+// var value = "2,4,6,8";
+// var user = prompt("Enter a Number At a Range of 1 To 10 ");
+// if (user === "2"){
+//     alert("Congratulation")
+// }else if(user !== "2" ){
+//     alert("Sorry! Try again!")
+// }else{
+//     alert("Thnks")
+// }
+
+// console.log(user)
